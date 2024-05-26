58ac55e27d7e3843080170ecf3f8019048e7a9e2
jonathane25 <161650447+jonathane25@users.noreply.github.com>
2024-05-26 00:48:44 +0000

    Made new File for quiz data

--- main.py
+++ main.py
@@ -10,4 +10,15 @@ current_question = 0
 score = 0
 return_to_quiz_flag = False
 
+#Function to Display the questions and choices
+def show_qustion():
+  global background_photo
+  #Get data from quiz_data
+  question = quiz_data[current_question]
+  qs_label.config(test=question["Question"])
+
+  #Show choices on buttons
+  choices = question["choices"]
+  for i in range(4):
+    choice_btns[i].config(text=choices[i], state="normal")
   
