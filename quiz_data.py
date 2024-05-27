e84304802442c76c5fdd86a5232cec7ee44ca61a
jonathane25 <161650447+jonathane25@users.noreply.github.com>
2024-05-27 02:44:42 +0000

    finished doing my quiz data questions and answers

---
+++ quiz_data.py
@@ -0,0 +1,62 @@
+quiz_data = {
+  {
+    "question": "What country is this?",
+    "choices": ["Australlia", "Japan", "New Zealand", "India"],
+    "answer": "India",
+    "background": "bg1.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["China", "United Kingdom", "Pakistan", "Sri Lanka"],
+    "answer": "Sri Lanka",
+    "background": "bg2.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Phillipines", "Thailand", "Japan", "South Korea"],
+    "answer": "Japan",
+    "background": "bg3.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Qatar", "Bahrain", "UAE", "Iraq"],
+    "answer": "Bahrain",
+    "background": "bg4.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Spain", "France", "Nigeria", "Egypt"],
+    "answer": "Spain",
+    "background": "bg5.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Mexico", "Romania", "Liechtenstein", "Ireland"],
+    "answer": "Liechtenstein",
+    "background": "bg6.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Nepal", "Indonesia", "Ecuador", "Monaco"],
+    "answer": "Monaco",
+    "background": "bg7.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Bhutan", "Nepal", "Tonga", "Mozambique"],
+    "answer": "Nepal",
+    "background": "bg8.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Cambodia", "Vietnam", "Kenya", "Malaysia"],
+    "answer": "Cambodia",
+    "background": "bg9.png"
+  },
+  {
+    "question": "What country is this?",
+    "choices": ["Hungary", "Estonia", "Germany", "Belgium"],
+    "answer": "Belgium",
+    "background": "bg10.png"
+  },
+  
