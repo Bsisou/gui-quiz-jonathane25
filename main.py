d7232cdf44bc6f374feadaf5c6d12230087df16a
jonathane25 <161650447+jonathane25@users.noreply.github.com>
2024-05-29 12:27:23 +0000

    Wrote the code for the function of the quiz (the points system, checking if user has selected correct answer, change background according to situation, doing the code for buttons, disabling buttons during certain situations (eg. when user gets the question right and can only click nect button) Code how the points work (if user gets question right its +1 point each time) and  also coded the end part where users final score was shown. Also Added the backgrounds for question 1-10, added the homepage  background and help background.

--- main.py
+++ main.py
@@ -21,4 +21,51 @@ def show_qustion():
   choices = question["choices"]
   for i in range(4):
     choice_btns[i].config(text=choices[i], state="normal")
+
+  #Clear feedback(correct or wrong) Disable Next button
+  feedback_label.config(text="")
+  next_btn.config(state ="disabled")
+
+  #Update backgroun image
+background_image = Image.open(question["background"])
+background_image = background_image.resize((1280, 720), Image.LANCZOS)
+background_photo = ImageTk.PhotoImage(background_image)
+quiz_background_label.config(image=background_photo)
+quiz_background_label.image = background_photo
+
+#Function to check the answer the user has selected and provide the user with feedback
+def check_answer(choice):
+  global score
+  #Get the current question from quiz_data.py
+  question = quiz_data[current_question]
+  selected_choice = choice_btns[choice].cget("text")
+
+  #Check if users selected answer matches with the correct answer
+  if selected_choice == question["answer"]:
+    score +=1
+    score_label.config(text="Score: {}/{}".format(score, len(quiz_data)))
+    feedback_lbael.config(text="Correct!", foreground="green")
+
+  else:
+    feedback_label.config(text="Incorrect!", foreground="red")
+
+  #Disable all the choice buttons and only allow the next button 
+  for button in choice_btns:
+    button.config(state="disabled")
+  next_btn.config(state="normal")
+
+#Move to Next Question
+def next_qestion():
+  global current_question
+  current_question += 1
+
+  if current_question < len(quiz_data):
+      #If there is more question show them
+      show_question()
+  else:
+      #if all questions have been answered by user show the final score and finish/end the quiz
+      messagebox.showinfo("Quiz Completed", "Quiz Completed! Fnal Score: {}/{}".format(score, len(quiz_data)))
+      root.destroy()
+
+#START Quiz (homepage)
   
